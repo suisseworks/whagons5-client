385cbf8b1f1c9f16d8d9a983ac8e88ddef567da0
Gabriel <malek.gabriel33@gmail.com>
2025-06-11 22:46:21 -0700

    prevent page unmounting for instant navigation and zero state loss

--- src/router/HomeRouter.tsx
+++ src/router/HomeRouter.tsx
@@ -1,28 +1,41 @@
-import { Navigate, Route, Routes } from 'react-router';
+import { Navigate, Route, Routes, useLocation } from 'react-router';
 
 import { DashBoardTask } from '../features/tasks';
 import { DashboardWorkplan } from '../features/workplan';
 import MainLayout from '../layouts/MainLayout';
-import { useParams } from 'react-router';
 import Stripe from '../pages/Stripe';
 import ChatWindow from '../features/aichat/pages/ChatWindow';
 import Profile from '@/pages/Profile';
+import { useMemo } from 'react';
 
+const pages = [
+  { path: '/tasks', component: <DashBoardTask /> },
+  { path: '/workplan', component: <DashboardWorkplan /> },
+  { path: '/stripe', component: <Stripe /> },
+  { path: '/ai-chat', component: <ChatWindow /> },
+  { path: '/profile', component: <Profile /> },
+];
 
+function AllPages() {
+  const location = useLocation();
+
+  const renderedPages = useMemo(() => pages.map(({ path, component }) => (
+    <div key={path} style={{ display: location.pathname.startsWith(path) ? 'block' : 'none', height: '100%' }}>
+      {component}
+    </div>
+  )), [location.pathname]);
+
+  return <>{renderedPages}</>;
+}
 
 export const HomeRoutes = () => {
 
   return (
     <>
       <MainLayout>
+        <AllPages />
         <Routes>
-          <Route path="/tasks" element={<DashBoardTask />} />
-          <Route path="/workplan" element={<DashboardWorkplan />} />
-          <Route path="/stripe" element={<Stripe />} />
-          <Route path="/ai-chat" element={<ChatWindow/>} />
-          <Route path="/profile" element={<Profile />} />
-
-          <Route path="/" element={<Navigate to="tasks" />} />
+          <Route path="/" element={<Navigate to="/tasks" replace />} />
         </Routes>
       </MainLayout>
     </>
