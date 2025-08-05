14bac68d9bccc046a6e5427dcd58446780f0bcc6
Gabriel <malek.gabriel33@gmail.com>
2025-08-05 10:45:35 -0700

    stuff

--- src/main.tsx
+++ src/main.tsx
@@ -4,19 +4,16 @@ import App from './App';
 import { AuthProvider } from './providers/AuthProvider';
 import { ThemeProvider } from './providers/ThemeProvider';
 import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
-import { persistor, store } from './store';
+import {store } from './store';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   // <React.StrictMode>
     <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
         <ThemeProvider defaultTheme="light" storageKey="whagons-ui-theme">
           <AuthProvider>
             <App />
           </AuthProvider>
         </ThemeProvider>
-      </PersistGate>
     </Provider>
   // {/* </React.StrictMode>, */}
 );
