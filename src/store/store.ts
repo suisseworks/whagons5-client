926758a73fdc480c35bfc16d59bae5bac857fe28
Gabriel <malek.gabriel33@gmail.com>
2025-08-05 10:42:31 -0700

    not persistent anymore

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,35 +1,23 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
-import { persistStore, persistReducer } from 'redux-persist';
 import { workspacesSlice } from "./reducers/workspacesSlice";
 import { teamsSlice } from "./reducers/teamsSlice";
 import { categoriesSlice } from "./reducers/categoriesSlice";
-import createIdbStorage from '@piotr-cz/redux-persist-idb-storage'
 import { tasksSlice } from "./reducers/tasksSlice";
 
-const persistConfig = {
-    key: 'root',
-    storage: createIdbStorage({ name: 'WhagonsAPP', storeName: 'redux' }),
-    version: 1,
-}
-
-const persistedReducer = persistReducer(persistConfig, 
-    combineReducers({
-        workspaces: workspacesSlice.reducer,
-        teams: teamsSlice.reducer,
-        categories: categoriesSlice.reducer,
-        tasks: tasksSlice.reducer
-    })
-)
+const rootReducer = combineReducers({
+    workspaces: workspacesSlice.reducer,
+    teams: teamsSlice.reducer,
+    categories: categoriesSlice.reducer,
+    tasks: tasksSlice.reducer
+});
 
 const store = configureStore({
-    reducer: persistedReducer,
+    reducer: rootReducer,
     middleware: (getDefaultMiddleware) => getDefaultMiddleware({
         serializableCheck: false
     }),
 });
 
-const persistor = persistStore(store);
-
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
-export { store, persistor };
+export { store };
